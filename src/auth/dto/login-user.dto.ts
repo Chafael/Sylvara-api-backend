48abb0fb6dfe4e841b2229a7894141d1b45a2604
Chafael <rafael_ruiz11@icloud.com>
2026-02-20 19:15:07 -0600

    actualicé el login con los campos que pidió el compañero

--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -3,9 +3,9 @@ import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 export class LoginUserDto {
     @IsEmail()
     @IsNotEmpty()
-    user_email: string;
+    email: string;
 
     @IsString()
     @IsNotEmpty()
-    user_password: string;
+    password: string;
 }
