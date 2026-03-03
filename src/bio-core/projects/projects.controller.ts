b06b41416f1124a4095420fea4e832fc614f73de
Chafael <rafael_ruiz11@icloud.com>
2026-03-03 17:07:19 -0600

    terminé el módulo de biodiversidad siguiendo el contrato de los JSON al 100%

--- src/bio-core/projects/projects.controller.ts
+++ src/bio-core/projects/projects.controller.ts
@@ -7,6 +7,7 @@ import {
     HttpStatus,
     Param,
     Post,
+    Request,
     UseGuards,
 } from '@nestjs/common';
 import { ProjectsService } from './projects.service';
@@ -19,23 +20,23 @@ export class ProjectsController {
     constructor(private readonly projectsService: ProjectsService) { }
 
     @Post()
-    create(@Body() dto: CreateProjectDto) {
-        return this.projectsService.create(dto);
+    create(@Body() dto: CreateProjectDto, @Request() req) {
+        return this.projectsService.create(dto, req.user.user_id);
     }
 
     @Get()
-    findAll() {
-        return this.projectsService.findAll();
+    findAll(@Request() req) {
+        return this.projectsService.findAll(req.user.user_id);
     }
 
     @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.projectsService.findOne(id);
+    findOne(@Param('id') id: string, @Request() req) {
+        return this.projectsService.findOne(id, req.user.user_id);
     }
 
     @Delete(':id')
     @HttpCode(HttpStatus.OK)
-    remove(@Param('id') id: string) {
-        return this.projectsService.remove(id);
+    remove(@Param('id') id: string, @Request() req) {
+        return this.projectsService.remove(id, req.user.user_id);
     }
 }
