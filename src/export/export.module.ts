41417819c4286d23e6781769afaf41ec7e553e30
Chafael <rafael_ruiz11@icloud.com>
2026-03-04 22:31:08 -0600

    Endpoint de reporte en JSON combinando PostgreSQL y MongoDB

--- src/export/export.module.ts
+++ src/export/export.module.ts
@@ -1,13 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { BigQueryService } from './bigquery.service';
 import { PdfService } from './services/pdf.service';
+import { ReportDataService } from './services/report-data.service';
 import { ExportController } from './export.controller';
-import { BioCoreModule } from '../bio-core/bio-core.module';
+
+import { SamplingPlot } from '../common/entities/sampling-plot.entity';
+import { User } from '../auth/entities/user.entity';
+import { SamplingPlot as MongoPlot, SamplingPlotSchema } from '../bio-core/projects/schemas/sampling-plot.schema';
 
 @Module({
-    imports: [BioCoreModule],
+    imports: [
+        TypeOrmModule.forFeature([SamplingPlot, User]),
+        MongooseModule.forFeature([{ name: MongoPlot.name, schema: SamplingPlotSchema }]),
+    ],
     controllers: [ExportController],
-    providers: [BigQueryService, PdfService],
-    exports: [BigQueryService, PdfService],
+    providers: [BigQueryService, PdfService, ReportDataService],
+    exports: [BigQueryService, PdfService, ReportDataService],
 })
 export class ExportModule { }
