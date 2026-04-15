6c144bf0101e0680acfb446e2f472b2082513324
uzzigg <243734@ids.upchiapas.edu.mx>
2026-04-14 20:28:34 -0600

    cambiando las cors

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+// src/main.ts
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
@@ -5,14 +6,22 @@ import { PROJECT_CONSTANTS } from './common/constants/project-constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     }),
   );
+
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   app.setGlobalPrefix(process.env.API_PREFIX ?? PROJECT_CONSTANTS.GLOBAL_API_PREFIX);
-  app.enableCors();
+
   const port = process.env.PORT || PROJECT_CONSTANTS.DEFAULT_PORT;
   await app.listen(port);
 }
